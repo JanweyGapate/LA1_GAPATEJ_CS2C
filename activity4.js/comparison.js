fc368315b36118b1df60298d37796c62aa26941d
JanweyGapate <janweygapate@gmail.com>
2024-09-23 07:52:38 +0800

    Create comparison.js

---
+++ activity4.js/comparison.js
@@ -0,0 +1,19 @@
+let firstFavNumb = 24;
+let seconFavNumb = 22;
+
+console.log(firstFavNumb > seconFavNumb)
+
+console.log(firstFavNumb < seconFavNumb)
+
+console.log(firstFavNumb >= seconFavNumb)
+
+console.log(firstFavNumb <= seconFavNumb)
+
+console.log(firstFavNumb === seconFavNumb)
+
+console.log(firstFavNumb == seconFavNumb)
+
+console.log(firstFavNumb !== seconFavNumb)
+
+console.log(firstFavNumb != seconFavNumb)
+            
