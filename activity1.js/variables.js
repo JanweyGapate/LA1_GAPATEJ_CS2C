db4415187bea2deb997eae08b85cfd5d85b07e57
JanweyGapate <janweygapate@gmail.com>
2024-09-23 07:40:00 +0800

    Create variables.js

---
+++ activity1.js/variables.js
@@ -0,0 +1,10 @@
+let Name ="Gapate Janwey";
+let whatDoYouWannaBeInYourLife = "Programmer";
+let gender = "male";
+let igHandle = "Nancy Kiray"
+
+console.log(Name)
+console.log(whatDoYouWannaBeInYourLife)
+console.log(gender)
+console.log(fbHandle)
+
