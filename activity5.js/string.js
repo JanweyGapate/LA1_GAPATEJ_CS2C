3af642d2780614b424a7ba3af6d21652a306f260
JanweyGapate <janweygapate@gmail.com>
2024-09-23 08:00:13 +0800

    Create string.js

---
+++ activity5.js/string.js
@@ -0,0 +1,8 @@
+let favActorFirstName = "Vice";
+let favActorLastName = " Ganda";
+let fullName = favActorFirstName + favActorLastName;          
+                    
+let message = "My favorite actor is " + fullName;
+message += " his best show is petrang kabayo"
+
+console.log(message.toUpperCase());
