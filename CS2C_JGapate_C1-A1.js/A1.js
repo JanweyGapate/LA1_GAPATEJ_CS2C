c79b5bf64e9bbdb7f7233b6771866c48fa13beff
JanweyGapate <janweygapate@gmail.com>
2024-09-24 17:55:32 +0800

    Create A1.js

---
+++ CS2C_JGapate_C1-A1.js/A1.js
@@ -0,0 +1,21 @@
+var Name = "[Janwey Molina Gapate]";
+var birthdate = "[June 22, 2005 ]";
+var birthPlace = "[Sabangan Bato,Galimuyod, Ilocos Sur, Phillipene 2710 ]";
+var addressed = "[Sabangan Bato, Galimuyod,Ilocos Sur,Phillipine 2709. ]";
+var Course = "[bachelor Of science in computer science ]";
+var Dream = "[Interface Designer]";
+var Year = "2";
+
+console.log (Name.toUpperCase() + "was born"+ birthdate+ "at" + birthPlace.toUpperCase() + "and currently living at "+ addressed.toUpperCase() + ".");
+console.log (Name.toLowerCase() + "is taking up + Course.toLowerCase() + "and dreams to be" + Dream + "after graduation");
+
+//student2 information
+var Name = "[Edith Inigo Valdez]";
+var birthdate = "[June 13, 2005 ]";
+var birthPlace = "[ Candon City, Ilocos Sur, Phillipene 2710 ]";
+var addressed = "[Patac, Galimuyod,Ilocos Sur,Phillipine 2709. ]";
+var Course = "[bachelor Of science in computer science ]";
+var Dream = "[web Developer]";
+var Year = "2";
+console.log (Name.toUpperCase() + "was born"+ "at" + bPlace.toUpperCase() + "and currently living at "+ addressed.toUpperCase() + ".");
+console.log (Name.toLowerCase() + "is taking up"+ bdate.toLowerCase()+ Course.toLowerCase() + "and dreams to be" + Dream + "after graduation");
